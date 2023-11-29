50a113aefca8955107a9b8c4ec7b5b0f621ef060
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-11-29 13:59:00 +0200

    portfolio-view.js fix

--- src/js/modules/portfolio-view.js
+++ src/js/modules/portfolio-view.js
@@ -4,19 +4,23 @@ const viewSwitchersWrapper = document.getElementById("view-switchers");
 const photosContent = document.getElementById("portfolio-photos-content");
 const listContent = document.getElementById("portfolio-list-content");
 
-function changeView(e) {
-  if (e.target.closest("#view-as-photo")) {
-    viewsListBtn.classList.remove("active-view-tigger");
-    viewsPhotosBtn.classList.add("active-view-tigger");
-    listContent.classList.remove("active-view");
-    photosContent.classList.add("active-view");
+
+if (viewsListBtn) {
+  function changeView(e) {
+    if (e.target.closest("#view-as-photo")) {
+      viewsListBtn.classList.remove("active-view-tigger");
+      viewsPhotosBtn.classList.add("active-view-tigger");
+      listContent.classList.remove("active-view");
+      photosContent.classList.add("active-view");
+    }
+    if (e.target.closest("#view-as-list")) {
+      viewsPhotosBtn.classList.remove("active-view-tigger");
+      viewsListBtn.classList.add("active-view-tigger");
+      photosContent.classList.remove("active-view");
+      listContent.classList.add("active-view");
+    }
   }
-  if (e.target.closest("#view-as-list")) {
-    viewsPhotosBtn.classList.remove("active-view-tigger");
-    viewsListBtn.classList.add("active-view-tigger");
-    photosContent.classList.remove("active-view");
-    listContent.classList.add("active-view");
-  }
+  
+  viewSwitchersWrapper.addEventListener("click", changeView);
 }
 
-viewSwitchersWrapper.addEventListener("click", changeView);
